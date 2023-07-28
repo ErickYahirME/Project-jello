25361e2c3b941803170564cadc9c44d5769429d0
Erick Yahir Meza Espina <espinayahir@gmail.com>
2023-07-28 14:21:02 -0600

    Creacion de login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegistroComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
